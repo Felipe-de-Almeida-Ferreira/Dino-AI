f8b487e8c855c4cc662b727d61068dcb29ca02cf
Felipe de Almeida Ferreira <felipealmeidaferreira5@gmail.com>
2025-01-15 16:50:00 -0300

    multiple dinos + NN(no parameter updates)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import random
+import numpy as np
 
 pygame.init()
 
@@ -36,13 +37,25 @@ BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
 pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
+class Layer():
+    def __init__(self, input_dim, output_dim, weights, bias, activation):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.weights = weights
+        self.biases = bias
+        self.activation = activation
+
+
+        self._activ_inp, self._activ_out = None, None
+
 class Dinossaur:
     X_POS = 80
     Y_POS = 310
     Y_POS_DUCK = 340
     JUMP_VEL = 8.5
-
+    
     def __init__(self):
+        self.layers = []
         self.duck_img = DUCKING
         self.run_img = RUNNING
         self.jump_img = JUMPING
@@ -58,7 +71,7 @@ class Dinossaur:
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
 
-    def update(self, userInput):
+    def update(self, userInput, x):
         if self.dino_duck:
             self.duck()
         if self.dino_run:
@@ -69,7 +82,7 @@ class Dinossaur:
         if self.step_index >=10:
             self.step_index = 0
 
-        if userInput[pygame.K_UP] and not self.dino_jump:
+        '''if userInput[pygame.K_UP] and not self.dino_jump:
             self.dino_duck = False
             self.dino_run = False
             self.dino_jump = True
@@ -78,6 +91,19 @@ class Dinossaur:
             self.dino_run = False
             self.dino_jump = False
         elif not (self.dino_jump or userInput[pygame.K_DOWN]):
+            self.dino_duck = False
+            self.dino_run = True
+            self.dino_jump = False'''
+        
+        if np.argmax(self.predict(x)) == 1 and not self.dino_jump:
+            self.dino_duck = False
+            self.dino_run = False
+            self.dino_jump = True
+        elif np.argmax(self.predict(x)) == 2 and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
+        elif np.argmax(self.predict(x)) == 2 and not (self.dino_jump or userInput[pygame.K_DOWN]):
             self.dino_duck = False
             self.dino_run = True
             self.dino_jump = False
@@ -104,6 +130,17 @@ class Dinossaur:
         if self.jump_vel < -self.JUMP_VEL:
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def predict(self, x):
+        return self.__feedforward(x)
+
+    def __feedforward(self, x):
+        self.layers[0].input = x
+        for current_layer, next_layer in zip(self.layers, self.layers[1:] + [Layer(0, 0, 0, 0, 0)]):
+            y = np.dot(current_layer.input, current_layer.weights.T) + current_layer.biases
+            current_layer._activ_inp = y
+            current_layer._activ_out = next_layer.input = current_layer.activation(y)
+        return self.layers[-1]._activ_out
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
@@ -157,17 +194,27 @@ class Bird(Obstacles):
         self.rect.y = 250
         self.index = 0
 
-        def draw(self, SCREEN):
-            if self.index >= 9:
-                self.index = 0
-            SCREEN.blit(self.image[self.index//5], self.rect)
-            self.index += 1
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index//5], self.rect)
+        self.index += 1
+
+def relu(x, derivative=False):
+    if derivative:
+        return np.where(x <= 0, 0, 1)
+    return np.maximum(0, x)
+
+def random_normal(rows, cols):
+    return np.random.randn(rows, cols)
+
+def ones(rows, cols):
+    return np.ones((rows, cols))
 
 def main():
-    global game_speed, x_pos_bg, y_pos_bg, points, obstacles
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, x, weights, biases
     run = True
     clock = pygame.time.Clock()
-    player = Dinossaur()
     cloud = Cloud()
     game_speed = 14
     x_pos_bg = 0
@@ -175,6 +222,15 @@ def main():
     points = 0
     font = pygame.font.Font('freesansbold.ttf', 20)
     obstacles = []
+    players = []
+    for i in range(20):
+        weights = random_normal(3, 3)
+        biases = ones(3, 3)
+        dino = Dinossaur()
+        dino.layers.append(Layer(input_dim=3, output_dim=3, weights=weights, bias=biases, activation=relu))
+        dino.layers.append(Layer(input_dim=3, output_dim=3, weights=weights, bias=biases, activation=relu))
+        dino.layers.append(Layer(input_dim=3, output_dim=3, weights=weights, bias=biases, activation=relu))
+        players.append(dino)
 
     def score():
         global points, game_speed
@@ -204,11 +260,20 @@ def main():
         SCREEN.fill((255, 255, 255))
         userInput = pygame.key.get_pressed()
 
-        player.draw(SCREEN)
-        player.update(userInput)
+
+        if len(obstacles) > 0:
+            obstacle = obstacles[0]
+            x = [game_speed, obstacle.rect.x, obstacle.rect.x]
+        else:
+            x = [game_speed, SCREEN_WIDTH, 0]
+
+        
+        for player in players:
+            player.update(userInput, x)
+            player.draw(SCREEN)
 
         if len(obstacles) == 0:
-            if random. randint(0, 2) == 0:
+            if random.randint(0, 2) == 0:
                 obstacles.append(SmallCactus(SMALL_CACTUS))
             elif random.randint(0, 2) == 1:
                 obstacles.append(LargeCactus(LARGE_CACTUS))
@@ -218,9 +283,12 @@ def main():
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
+            
+            for player in players:
+                if player.dino_rect.colliderect(obstacle.rect):
+                        pygame.draw.rect(SCREEN, (255, 0, 0), player.dino_rect, 2)
 
-            if player.dino_rect.colliderect(obstacle.rect):
-                pygame.draw.rect(SCREEN, (255, 0, 0), player.dino_rect, 2)
+ 
 
         background()
         
